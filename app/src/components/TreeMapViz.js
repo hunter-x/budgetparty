d7a0742787be6283a16913c4730da41abd3f09de
Abderrahmen Gharsallah <aa.test20@gmail.com>
2017-09-12 11:36:47 +0100

    adding budget treemap in the welcome

--- app/src/components/TreeMapViz.js
+++ app/src/components/TreeMapViz.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom'
 import Translate    from 'react-translate-component';
 const _t = Translate.translate;
+import TreeMap from "react-d3-treemap";
+// Include its styles in you build process as well
+import "react-d3-treemap/dist/react.d3.treemap.css";
+import data from "../data/treemap"
 
 class TreeMapViz extends Component {
+
   render() {
+
     return (
       <div className="intro">
         <Link to="/dashboard" className="intro__skip">{_t('intro.skip')}</Link>
         <div className="intro__image-wrapper">
-          hhhh
+        <TreeMap
+        height={400}
+        width={400}
+        data={data}
+        valueUnit={"dt"}
+    />
         </div>
 
         <div className="intro__button-flex-container">
---
+++ app/src/data/treemap.js
@@ -0,0 +1,130 @@
+const treemapdata =  {
+
+          
+              "name": "analytics",
+              "children": [
+                  {
+                      "name": "chaussées et pistes",
+                      "children": [
+                        {
+                          "value": "1030812",
+                          "name": "revêtement des chausées"
+                      },
+                      {
+                          "value": "60940",
+                          "name": "construction des trottoirs"
+                      },
+                      {
+                          "value": "60000",
+                          "name": "travaux d'entretien et de maintenance"
+                      }
+                      ]
+                  },
+                  {
+                      "name": "éclairage",
+                      "children": [
+                        {
+                          "value": "103999",
+                          "name": "l'éclairage public"
+                      },
+                      {
+                          "value": "17164",
+                          "name": "travaux d'entretien et de maintenance"
+                      },
+                      {
+                          "value": "10434",
+                          "name": "installation des régulateurs de tension"
+                      }
+                      ]
+                  },
+                  {
+                    "name": "bâtiments administratifs",
+                    "children": [
+                      {
+                        "value": "659139",
+                        "name": "hôtel de ville"
+                    },
+                    {
+                        "value": "140000",
+                        "name": "arrondissement municipal"
+                    },
+                    {
+                        "value": "90652",
+                        "name": "fourrière municipale"
+                    },
+                    {
+                        "value": "5375",
+                        "name": "construction et aménagement des bâtiments à caractère économique"
+                    }
+                    ]
+                },
+                {
+                  "name": "études",
+                  "children": [
+                    {
+                      "value": "120000",
+                      "name": "études des shémas de circulation"
+                    },
+                    {
+                        "value": "20000",
+                        "name": "études des plan d'aménagement urbain"
+                    },
+                    {
+                        "value": "19880",
+                        "name": "autres études"
+                    } 
+                  ]
+              },
+              {
+                "name": "Equipments Acquisition",
+                "children": [
+                  {
+                    "value": "189595",
+                    "name": "acquisition de matériels de nettoiement et des chaussées"
+                  },
+                  {
+                      "value": "26646",
+                      "name": "programmes et équipements informatiques"
+                  },
+                  {
+                    "value": "137414",
+                    "name": "acquiqition des moyens de transport"
+                  }
+                ]
+              },
+              {
+                "name": "Rehabilitation et Aménagement",
+                "children": [
+                  {
+                      "value": "14252",
+                      "name": "dépenses diverses"
+                  },
+                  {
+                      "value": "8208",
+                      "name": "aménagement espaces verts et entrées des villes"
+                  },
+                  {
+                      "value": "250000",
+                      "name": "acquisition de terrains"
+                  }
+                ]
+            },
+            {
+              "name": "Sport et Culture",
+              "children": [
+                {
+                  "value": "57142",
+                  "name": "construction et aménagement des infrastructure sportifs"
+              },
+              {
+                  "value": "1,052",
+                  "name": "construction et aménagement d'autre instalations publiques"
+              }
+              ]
+          },
+              ]
+         
+     
+    }
+
+export default treemapdata;
